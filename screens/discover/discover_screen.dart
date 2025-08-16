d147ef86f52a57ad44293501162f3a19c8b1830d
liangxingguang <liangxingguang@gmail.com>
2025-08-16 17:07:11 +0800

    feat: add discover empty page

---
+++ screens/discover/discover_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class DiscoverScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('发现', style: TextStyle(fontSize: 24)));
+  }
+}
