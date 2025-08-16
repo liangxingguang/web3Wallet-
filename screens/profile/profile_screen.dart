be0620c30c8ed0c3d561548f873f63c834370e6e
liangxingguang <liangxingguang@gmail.com>
2025-08-16 17:07:20 +0800

    feat: add profile empty page

---
+++ screens/profile/profile_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('我的', style: TextStyle(fontSize: 24)));
+  }
+}
